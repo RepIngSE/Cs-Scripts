d98bb8daab40c6be648373b96659af8fc358ee39
RepIngSE <139818962+RepIngSE@users.noreply.github.com>
2023-08-13 15:07:42 -0500

    Create

---
+++ College/Basics/Method_ToUpper.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Ejercicio18
+{
+    class Metodos
+    {
+        static void Main(string[] args)
+        {
+            // Definir una cadena de texto
+            string Algo = "Hello, my name is Juan, Hello, how are you?";
+
+            // Mostrar la cadena original
+            Console.WriteLine(Algo + "\n");
+
+            // Convertir la cadena a mayúsculas y mostrarla
+            Console.WriteLine(Algo.ToUpper());
+        }
+    }
+}
